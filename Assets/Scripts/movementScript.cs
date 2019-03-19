0cf7a5cc9c8298d907ff54e8520ab2256c5425bd
Lucas <lucasrchaves1@gmail.com>
2019-03-19 16:13:42 -0300

    functionalities implemented in the script(movementScript) for the W,A,S,D buttons

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -16,6 +16,22 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("W"))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetButton("S"))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetButton("A"))
+        {
+            transform.Rotate(0, rotacionSpeed, 0);
+        }
+        if (Input.GetButton("D"))
+        {
+            transform.Rotate(0, -rotacionSpeed, 0);
+        }
+
     }
 }
