d70a690609782d219eb7a939253249902952cda0
Lucas <lucasrchaves1@gmail.com>
2019-03-19 15:59:35 -0300

    Create archive movementScript and add configuration buttons W,A,S,D

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotacionSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 1.0f;
+        rotacionSpeed = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
