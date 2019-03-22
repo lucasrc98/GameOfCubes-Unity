852754450d58b29cc10754c0405f8364222464ec
Lucas <lucasrchaves1@gmail.com>
2019-03-22 14:06:53 -0300

    Infinite Jump

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -6,39 +6,38 @@ public class movementScript : MonoBehaviour
 {
     public float movementSpeed;
     public float rotacionSpeed;
+    public float forceJump;
     // Start is called before the first frame update
     void Start()
     {
         movementSpeed = 0.2f;
         rotacionSpeed = 3.0f;
+        forceJump = 80f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
+        if (Input.GetKey(KeyCode.W)){
             transform.Translate(0, 0, movementSpeed);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.S)){
             transform.Translate(0, 0, -movementSpeed);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)){
             transform.Rotate(0, rotacionSpeed, 0);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)){
             transform.Rotate(0, -rotacionSpeed, 0);
         }
-        if (Input.GetKey(KeyCode.E))
-        {
+        if (Input.GetKey(KeyCode.E)){
             this.gameObject.transform.position += this.gameObject.transform.right * movementSpeed;
         }
-        if (Input.GetKey(KeyCode.Q))
-        {
+        if (Input.GetKey(KeyCode.Q)){
             this.gameObject.transform.position += this.gameObject.transform.right * -movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.Space)){
+            this.gameObject.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.up * forceJump);
         }
 
     }
