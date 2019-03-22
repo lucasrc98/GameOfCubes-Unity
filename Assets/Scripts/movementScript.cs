376db8f374db1e146ec46fb4d386f916e8b2cf69
Lucas <lucasrchaves1@gmail.com>
2019-03-22 14:28:33 -0300

    Real jumping

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -7,12 +7,14 @@ public class movementScript : MonoBehaviour
     public float movementSpeed;
     public float rotacionSpeed;
     public float forceJump;
+    bool jumping;
     // Start is called before the first frame update
     void Start()
     {
         movementSpeed = 0.2f;
         rotacionSpeed = 3.0f;
         forceJump = 80f;
+        jumping = false;
     }
 
     // Update is called once per frame
@@ -36,9 +38,29 @@ public class movementScript : MonoBehaviour
         if (Input.GetKey(KeyCode.Q)){
             this.gameObject.transform.position += this.gameObject.transform.right * -movementSpeed;
         }
-        if (Input.GetKey(KeyCode.Space)){
+
+        if (Input.GetKey(KeyCode.Space) && !jumping){
             this.gameObject.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.up * forceJump);
         }
+       
 
     }
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.tag == "floor")
+            {
+                jumping = false;
+                Debug.Log("jumping false");
+            }
+        }
+
+        void OnCollisionExit(Collision col)
+        {
+            if (col.gameObject.tag == "floor")
+            {
+                jumping = true;
+                Debug.Log("jumping true");
+            }
+        }
 }
