e60de1b507d3640e5f547012eb19f1eef37eec53
Lucas <lucasrchaves1@gmail.com>
2019-03-29 16:05:11 -0300

    Indenting code

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movementScript : MonoBehaviour
-{
+public class movementScript : MonoBehaviour{
     public float movementSpeed;
     public float rotacionSpeed;
     public float forceJump;
     bool jumping;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         movementSpeed = 0.2f;
         rotacionSpeed = 3.0f;
         forceJump = 80f;
@@ -18,23 +17,27 @@ public class movementScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         if (Input.GetKey(KeyCode.W)){
             transform.Translate(0, 0, movementSpeed);
         }
+
         if (Input.GetKey(KeyCode.S)){
             transform.Translate(0, 0, -movementSpeed);
         }
+
         if (Input.GetKey(KeyCode.A)){
             transform.Rotate(0, rotacionSpeed, 0);
         }
+
         if (Input.GetKey(KeyCode.D)){
             transform.Rotate(0, -rotacionSpeed, 0);
         }
+
         if (Input.GetKey(KeyCode.E)){
             this.gameObject.transform.position += this.gameObject.transform.right * movementSpeed;
         }
+
         if (Input.GetKey(KeyCode.Q)){
             this.gameObject.transform.position += this.gameObject.transform.right * -movementSpeed;
         }
@@ -42,6 +45,7 @@ public class movementScript : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && !jumping){
             this.gameObject.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.up * forceJump);
         }
+
         if (this.gameObject.transform.position.y < -10){
             this.gameObject.transform.position = new Vector3(0, 3, 0);
 
@@ -49,9 +53,7 @@ public class movementScript : MonoBehaviour
        
 
     }
-
-        void OnCollisionEnter(Collision col)
-        {
+        void OnCollisionEnter(Collision col){
             if (col.gameObject.tag == "floor")
             {
                 jumping = false;
@@ -59,18 +61,16 @@ public class movementScript : MonoBehaviour
             }
         }
 
-        void OnCollisionExit(Collision col)
-        {
-            if (col.gameObject.tag == "floor")
-            {
+        void OnCollisionExit(Collision col){
+            if (col.gameObject.tag == "floor"){
                 jumping = true;
                 Debug.Log("jumping true");
             }
         }
 
-    private void OnCollisionStay(Collision col){
-        if (col.gameObject.tag == "maca"){
-            Destroy(col.gameObject);
+        private void OnCollisionStay(Collision col){
+            if (col.gameObject.tag == "maca"){
+                Destroy(col.gameObject);
+            }
         }
-    }
 }
